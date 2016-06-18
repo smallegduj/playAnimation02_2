5349578e854c2bda75ab8f04f892617fb7e8fdc1
MikeWu <MikeWu@mikewude-macbook-pro.local>
2016-06-18 22:33:02 +0800

    play an animation by keyframe

--- playAnimation02_2/ViewController.swift
+++ playAnimation02_2/ViewController.swift
@@ -1,15 +1,10 @@
-//
-//  ViewController.swift
-//  playAnimation02_2
-//
-//  Created by MikeWu on 2016/6/18.
-//  Copyright © 2016年 MSMedia. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var arrow: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +15,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func play(sender: AnyObject) {
+
+        UIView.animateKeyframesWithDuration(4, delay: 0, options: [], animations: {
+            
+            UIView.addKeyframeWithRelativeStartTime(0, relativeDuration: 0.25, animations: {
+                self.arrow.center.x += 200
+            })
+            UIView.addKeyframeWithRelativeStartTime(0.25, relativeDuration: 0.25, animations: {
+                self.arrow.center.y += 100
+            })
+            UIView.addKeyframeWithRelativeStartTime(0.5, relativeDuration: 0.25, animations: {
+                self.arrow.center.x -= 200
+            })
+            UIView.addKeyframeWithRelativeStartTime(0.75, relativeDuration: 0.25, animations: {
+                self.arrow.center.y -= 100
+            })
+            }, completion: nil)
+    }
 
 }
 
